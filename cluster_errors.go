282556907c1d24cf36677ac83b708bb8f6b264e8
Martin Beauvais <mbeauv@gmail.com>
2026-07-16 22:25:18 -0400

    feat: expose cluster error helpers

--- cluster_errors.go
+++ cluster_errors.go
@@ -1,18 +1,31 @@
 package mycel
 
 import (
+	"fmt"
+
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 const (
-	NotPrimaryReason   = "MYCEL_CLUSTER_NOT_PRIMARY"
-	PrimaryNodeIDKey   = "mycel-primary-node-id"
-	PrimaryNodeNameKey = "mycel-primary-node-name"
-	PrimaryBackendKey  = "mycel-primary-backend-advertise-addr"
-	AuthorityEpochKey  = "mycel-authority-epoch"
+	NotPrimaryReason       = "MYCEL_CLUSTER_NOT_PRIMARY"
+	SnapshotRequiredReason = "MYCEL_WAL_SNAPSHOT_REQUIRED"
+	PrimaryNodeIDKey       = "mycel-primary-node-id"
+	PrimaryNodeNameKey     = "mycel-primary-node-name"
+	PrimaryBackendKey      = "mycel-primary-backend-advertise-addr"
+	AuthorityEpochKey      = "mycel-authority-epoch"
 )
+
+type SnapshotRequiredInfo struct {
+	RequestedAfterLSN uint64
+	NextRequestedLSN  uint64
+	FirstRetainedLSN  uint64
+	LastCommittedLSN  uint64
+	CheckpointLSN     uint64
+	PrimaryNodeID     string
+	AuthorityEpoch    string
+}
 
 type PrimaryHint struct {
 	NodeID               string
@@ -24,6 +37,28 @@ type PrimaryHint struct {
 func IsNotPrimaryError(err error) bool {
 	st, ok := status.FromError(err)
 	return ok && st.Code() == codes.FailedPrecondition && st.Message() == "node is not cluster primary"
+}
+
+func IsSnapshotRequiredError(err error) bool {
+	st, ok := status.FromError(err)
+	return ok && st.Code() == codes.FailedPrecondition && st.Message() == "follower requires snapshot catch-up"
+}
+
+func SnapshotRequiredInfoFromError(err error) (SnapshotRequiredInfo, bool) {
+	st, ok := status.FromError(err)
+	if !ok {
+		return SnapshotRequiredInfo{}, false
+	}
+	for _, detail := range st.Details() {
+		info, ok := detail.(*errdetails.ErrorInfo)
+		if !ok || info.GetReason() != SnapshotRequiredReason {
+			continue
+		}
+		md := info.GetMetadata()
+		parse := func(k string) uint64 { var v uint64; fmt.Sscanf(md[k], "%d", &v); return v }
+		return SnapshotRequiredInfo{RequestedAfterLSN: parse("requested_after_lsn"), NextRequestedLSN: parse("next_requested_lsn"), FirstRetainedLSN: parse("first_retained_lsn"), LastCommittedLSN: parse("last_committed_lsn"), CheckpointLSN: parse("checkpoint_lsn"), PrimaryNodeID: md["primary_node_id"], AuthorityEpoch: md["authority_epoch"]}, true
+	}
+	return SnapshotRequiredInfo{}, false
 }
 
 func PrimaryHintFromError(err error) (PrimaryHint, bool) {
